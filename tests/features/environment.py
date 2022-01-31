86e8c073ec60b8014f10bc516e49bf4449606158
cscomic <cscomicanimation@gmail.com>
2022-01-31 17:15:17 +0800

    init project

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-'''
+"""
 @File    :   environment.py
 @Time    :   2022/01/30 17:59:01
 @Author  :   Tim Chen
@@ -8,6 +8,23 @@
 @Contact :   cscomicanimation@gmail.com
 @License :   MIT
 @Desc    :   None
-'''
+"""
 
 # here put the import lib
+from typing import NoReturn
+from behave.log_capture import capture
+
+from behave.runner import Context
+
+
+def before_all(context: Context) -> NoReturn:
+    """
+
+    """
+    # context.config.setup_logging(configfile="behave_logging.ini")
+    pass
+
+
+@capture
+def after_all(context: Context) -> NoReturn:
+    pass
--- tests/features/steps/test_step.py
+++ tests/features/steps/test_step.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-'''
+"""
 @File    :   test_step.py
 @Time    :   2022/01/30 18:02:45
 @Author  :   Tim Chen
@@ -8,13 +8,15 @@
 @Contact :   cscomicanimation@gmail.com
 @License :   MIT
 @Desc    :   None
-'''
+"""
 
 # here put the import lib
 from behave.runner import Context
 from behave import *
 from typing import NoReturn
 
+
 @given('Hello world')
 def step_impl(context: Context) -> NoReturn:
     print("Hello World!\n\n")
+
